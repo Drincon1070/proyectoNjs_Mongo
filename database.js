e39f98f580b1b578f018043c345db085c494e37f
drinconle1 <drincon.89@gmail.com>
2022-11-03 21:08:12 -0500

    Commit: base de datos remota Ok

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongo = require("mongoose");
 
 (async () => {
     try {
-        const db = await mongo.connect("mongodb://localhost:27017/dbPrueba1"); 
+        const db = await mongo.connect("mongodb+srv://drinconle:ypGJw4RzRuGe7qKU@cluster0.ftofspf.mongodb.net/minticprueba1?retryWrites=true&w=majority"); 
         console.log("Conexión establecida en: " + db.connection.name); 
     } catch (error) {
         console.error(error); 
